b96082bdff4f47b11064559b15cff7ab75a8b3a3
tarnished moth <87274781+tarnishedmoth@users.noreply.github.com>
2026-07-20 15:53:20 -0400

    misc: interactable click link/scene config check for clarity

--- scripts/click_link.gd
+++ scripts/click_link.gd
@@ -1,7 +1,18 @@
 extends Node3D
 
-@export var url := "https://itch.io/c/188585/"
+const DEVPODS_COLLECTION := "https://itch.io/c/188585/"
+
+@export var url: String
 
 func open_link():
-	OS.shell_open(url)
+	var _url: String
+	if not url.is_empty():
+		_url = url
+	else:
+		print("Empty URL on click_link.gd node %s.
+		Configure property in Inspector.
+		Defaulting to DevPods Itch.io collection.")
+		_url = DEVPODS_COLLECTION
+	
+	OS.shell_open(_url)
 	JavaScriptBridge.eval("window.open('%s', '_blank');" % url)
--- scripts/click_scene_change.gd
+++ scripts/click_scene_change.gd
@@ -1,8 +1,12 @@
 extends StaticBody3D
 
 @export_file("*.tscn")
-var goto_scene := ""
+var goto_scene: String = ""
 
 func open_scene():
+	if goto_scene.is_empty():
+		push_warning("Empty goto_scene property for click_scene_change.gd node. Configure in Inspector.")
+		return
+	
 	print("Changing scene (did file change?) to:", goto_scene)
 	get_tree().change_scene_to_file(goto_scene)
