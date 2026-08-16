cab341a23602cdcab41b75c34087cda2297a4f0f
Link of Origin <jesselink333@gmail.com>
2026-08-16 16:17:19 -0600

    Scaffolding for settings UI (not yet implemented)

---
+++ ui/settings_screen/controls/controls_screen.gd
@@ -0,0 +1,2 @@
+class_name ControlSettingsScreen
+extends PanelContainer
---
+++ ui/settings_screen/settings_screen.gd
@@ -0,0 +1,5 @@
+class_name SettingsScreen
+extends PanelContainer
+
+## Set to 'false' to hide the heading label (eg. for when inside a TabContainer)
+@export var show_heading: bool = true
