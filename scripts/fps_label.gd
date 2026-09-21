2a4ddb6405c6da1d19411f04425ff041d689a2cf
Christer Kaitila <breakdance@mcfunkypants.com>
2026-09-21 16:13:16 -0700

    FPS display also shows MB vram used

--- scripts/fps_label.gd
+++ scripts/fps_label.gd
@@ -1,4 +1,6 @@
 extends Label
 
 func _process(_delta: float) -> void:
-	text = "FPS: %d" % Engine.get_frames_per_second()
+	# measure how much texture ram we are using on the graphics card
+	var vram_used_mb = Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED) / (1024 * 1024)
+	text = "%d" % Engine.get_frames_per_second() + "FPS" + " - VRAM used: %d" % vram_used_mb + "MB"
